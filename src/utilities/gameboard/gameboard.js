65b77a475417742573ef47fa132922ffb0ab9654
Brian <bbbrianboylen@gmail.com>
2021-02-24 10:21:02 -0600

    added logic to place ships

--- src/utilities/gameboard/gameboard.js
+++ src/utilities/gameboard/gameboard.js
@@ -1,6 +1,24 @@
+import Ship from '../ship/ship';
+
 const Gameboard = () => {
-  const grid = () => {
-    
+  const gridPlacements = {};
+  const shipIndexes = {};
+
+  const placeShip = (ship, coordinates, gridPlacements, shipIndexes) => {
+    for (let coord of coordinates) {
+      gridPlacements[coord] = ship;
+      if (!shipIndexes[ship]) {
+        shipIndexes[ship] = [coord];
+      } else {
+        shipIndexes[ship].push(coord);
+      }
+    }
+  }
+
+  return {
+    placeShip,
+    gridPlacements,
+    shipIndexes
   }
 }
 export default Gameboard;
--- src/utilities/gameboard/gameboard.test.js
+++ src/utilities/gameboard/gameboard.test.js
@@ -1,5 +1,11 @@
 import Gameboard from './gameboard';
 
 test('places ships at specific coordinates',() => {
-  
+  const ship = {};
+  const coordinates = ['A1','A2','A3'];
+  const gridPlacements = {};
+  const shipIndexes = {};
+  Gameboard().placeShip(ship, coordinates, gridPlacements, shipIndexes);
+  expect(gridPlacements).toEqual({'A1': ship, 'A2': ship, 'A3': ship})
+  expect(shipIndexes).toEqual({[ship]: ['A1','A2','A3'] })
 })
